4bd7cd51c45c1af28d68af1d81c1733047f2a61a
Mspaenle <Mahe.spaenle@etu.umontpellier.fr>
2017-01-19 15:57:06 +0100

    Update cv2_camera.py

--- g2/dalger-spaenle/photobooth/scripts/cv2_camera.py
+++ g2/dalger-spaenle/photobooth/scripts/cv2_camera.py
@@ -3,12 +3,9 @@ import numpy as np
 import cv2
 
 class Camera:
-    '''
-    Thin wrapper for the cv2 camera interface to make it look like a PiCamera
-    '''
     def __init__(self):
         self.cam = cv2.VideoCapture(0)
-        time.sleep(.3) ## wait for auto adjust
+        time.sleep(.3)
         self.led = False
         self.previewing = False
         
@@ -36,18 +33,18 @@ def test():
     cap = cv2.VideoCapture(0)
 
     while(True):
-        # Capture frame-by-frame
+        
         ret, frame = cap.read()
 
-        # Our operations on the frame come here
+
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        # Display the resulting frame
+
         cv2.imshow('frame',gray)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-    # When everything done, release the capture
+
     cap.release()
     cv2.destroyAllWindows()
 
